5d63d4ff363148e1d5b088c68746e1366205d052
Michael D. Smith <michael.smith.erdc@gmail.com>
2013-08-20 12:58:25 -0400

    change order

--- paintstore/widgets.py
+++ paintstore/widgets.py
@@ -10,8 +10,8 @@ class ColorPickerWidget(forms.TextInput):
         }
         
         js  = (
-                 ("%s/%s" % (settings.STATIC_URL, "paintstore/colorpicker.js")),
-                 ("%s/%s" % (settings.STATIC_URL, "paintstore/jquery_1.7.2.js"))
+                 ("%s/%s" % (settings.STATIC_URL, "paintstore/jquery_1.7.2.js")),
+                 ("%s/%s" % (settings.STATIC_URL, "paintstore/colorpicker.js"))
         )
         
     input_type = 'colorpicker'
