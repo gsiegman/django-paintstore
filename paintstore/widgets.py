4b4b68aa8693a06741f09e9ce3fdc03c4caa42fb
Michael D. Smith <michael.smith.erdc@gmail.com>
2013-08-20 12:36:10 -0400

    add jquery 1.7.2 link

--- paintstore/widgets.py
+++ paintstore/widgets.py
@@ -10,7 +10,7 @@ class ColorPickerWidget(forms.TextInput):
         }
         
         js  = (
-            "%s/%s" % (settings.STATIC_URL,  "paintstore/colorpicker.js"),
+            "%s/%s" % (settings.STATIC_URL,  "paintstore/colorpicker.js", "paintstore/jquery_1.7.2.js"),
         )
 
     input_type = 'colorpicker'
