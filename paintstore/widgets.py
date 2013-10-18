ea2d582e5170f3ed50882539dad6f27d9f341537
Glenn Siegman <gsiegman@gsiegman.com>
2013-10-17 20:46:41 -0400

    minor updates to match coding style

--- paintstore/widgets.py
+++ paintstore/widgets.py
@@ -5,15 +5,15 @@ from django.utils.safestring import mark_safe
 
 class ColorPickerWidget(forms.TextInput):
     class Media:
-        css = { 
-            "all": ("%s/%s" %  (settings.STATIC_URL, "paintstore/css/colorpicker.css"),)
+        css = {
+            "all": ("%s/%s" % (settings.STATIC_URL, "paintstore/css/colorpicker.css"),)
         }
-        
+
         js  = (
-                 ("%s/%s" % (settings.STATIC_URL, "paintstore/jquery_1.7.2.js")),
-                 ("%s/%s" % (settings.STATIC_URL, "paintstore/colorpicker.js"))
+            ("%s/%s" % (settings.STATIC_URL, "paintstore/jquery_1.7.2.js")),
+            ("%s/%s" % (settings.STATIC_URL, "paintstore/colorpicker.js"))
         )
-        
+
     input_type = 'colorpicker'
 
     def render(self, name, value, attrs=None):
